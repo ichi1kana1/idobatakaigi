968c08e88f396a88f95b681f4e491ef78fd0be32
市村果南 <ichimurakana@kn1mr.local>
2021-03-21 11:02:03 +0900

    apply grid layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
-
+import { SettingsSystemDaydreamOutlined } from '@material-ui/icons';
+import React, { useState } from 'react';
+import Main from './Main';
 import SignIn from './SignIn';
+import config from '../config.json';
 
 export default () => {
-  return <SignIn/>;
+  const [name, setName] = useState('');
+
+  if(config.signInEnabled && name === ''){
+    return <SignIn setName={setName}/>;
+  }else{
+    return <Main name={name}/>;
+  }
+  
+  
 };
