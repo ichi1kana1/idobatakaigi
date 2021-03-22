2df91a9289fcd1d93fe0c728228b0900ad3bf983
市村果南 <ichimurakana@kn1mr.local>
2021-03-22 11:54:51 +0900

    display messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { List } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { messagesRef } from '../firebase';
+import MessageItem from './MessageItem';
 
 const useStyles = makeStyles({
     root: {
         gridRow: 1,
+        overflow: 'auto',
+        width: '100%',
     },
 })
 
@@ -14,22 +18,30 @@ const MessageList = () => {
 
     useEffect(() => {
         messagesRef
-        .orderByKey()
-        .limitToLast(3)
-        .on('value', (snapshot) => {
-            const messages = snapshot.val();
-            if ( messages === null ) return;
-            const entries = Object.entries(messages);
-            const newMessages = entries.map(entry => {
-                const [key, nameAndText] = entry;
-                return { key, ...nameAndText };
+            .orderByKey()
+            .limitToLast(15)
+            .on('value', (snapshot) => {
+                const messages = snapshot.val();
+                if (messages === null) return;
+                const entries = Object.entries(messages);
+                const newMessages = entries.map(entry => {
+                    const [key, nameAndText] = entry;
+                    return { key, ...nameAndText };
+                });
+                setMessages(newMessages);
             });
-            setMessages(newMessages);
-        });
     }, []);
 
     return (
-        <div className={classes.root}>MessageList</div>
+        <List className={classes.root}>
+            { messages.map(({ key, name, text }) => {
+                return (
+                    <MessageItem key={key} name={name} text={text}>
+                        item
+                    </MessageItem>
+                );
+            })}
+        </List>
     );
 };
 export default MessageList;
