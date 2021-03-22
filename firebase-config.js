700496df7129b5b91b4709eac0e343d817776908
市村果南 <ichimurakana@kn1mr.local>
2021-03-22 14:39:36 +0900

    create command to generate .env.local

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyD4OnuZA8QA-VaWuiHKdt4RFDOkwEDbykI",
+    authDomain: "chatty-9112c.firebaseapp.com",
+    databaseURL: "https://chatty-9112c-default-rtdb.firebaseio.com",
+    projectId: "chatty-9112c",
+    storageBucket: "chatty-9112c.appspot.com",
+    messagingSenderId: "397512256672",
+    appId: "1:397512256672:web:a1e9c70d5f92ccddd2edc5"
+};
